f982da7b358957c92d36f5924044d083ddf16ec7
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2020-05-25 17:08:30 +0300

    extraxt buy order partial matching without residual buy order box test;

--- contracts/src/test/scala/org/ergoplatform/contracts/tests/dex/DexBuyOrderPartialMatchTest.scala
+++ contracts/src/test/scala/org/ergoplatform/contracts/tests/dex/DexBuyOrderPartialMatchTest.scala
@@ -123,29 +123,93 @@ class DexLimitOrderTest
         println(s"buy order contract cost: $cost")
         cost should be < buyerContractCostLimit
 
-        // TODO: extract
-        // buy order should fail without residual buy order box
-        val txWithoutResidualBuyOrder = new ErgoLikeTransaction(
-          IndexedSeq(sellOrderBox).map(b => Input(b.id, ProverResult.empty)),
-          IndexedSeq(),
-          IndexedSeq(returnBox)
-        )
-        verifier
-          .verify(
-            buyOrderBox.ergoTree,
-            ErgoLikeContextTesting(
-              currentHeight       = 0,
-              lastBlockUtxoRoot   = AvlTreeData.dummy,
-              minerPubkey         = ErgoLikeContextTesting.dummyPubkey,
-              boxesToSpend        = IndexedSeq(sellOrderBox, buyOrderBox),
-              spendingTransaction = txWithoutResidualBuyOrder,
-              self                = buyOrderBox
-            ),
-            ProverResult.empty,
-            fakeMessage
-          )
-          .get
-          ._1 shouldBe false
     }
+  }
+
+  property("buy order partial match without residual buy order box") {
+    val verifier = new ErgoLikeTestInterpreter
+    val buyerPk =
+      (new ContextEnrichingTestProvingInterpreter).dlogSecrets.head.publicImage
+    val sellerPk =
+      (new ContextEnrichingTestProvingInterpreter).dlogSecrets.head.publicImage
+
+    val tokenId        = tokenIdGen.sample.get
+    val tokenPrice     = 20000000L
+    val dexFeePerToken = 1000000L
+    val tokenAmount    = 99L
+
+    val buyOrderContractParams =
+      DexBuyerContractParameters(buyerPk, tokenId, tokenPrice, dexFeePerToken)
+
+    val buyOrderContract =
+      DexLimitOrderContracts.buyerContractInstance(buyOrderContractParams)
+
+    val buyOrderBox = ErgoBox(
+      value          = tokenAmount * (tokenPrice + dexFeePerToken),
+      ergoTree       = buyOrderContract.ergoTree,
+      creationHeight = 0
+    )
+
+    val partialMatchTokenAmount = tokenAmount / 2
+
+    val residualBuyOrderBox = ErgoBox(
+      value          = (tokenAmount - partialMatchTokenAmount) * (tokenPrice + dexFeePerToken),
+      ergoTree       = buyOrderContract.ergoTree,
+      creationHeight = 0,
+      additionalRegisters = Map(
+        ErgoBox.R4 -> ByteArrayConstant(tokenId),
+        ErgoBox.R5 -> LongConstant(tokenPrice),
+        ErgoBox.R6 -> LongConstant(dexFeePerToken),
+        ErgoBox.R7 -> ByteArrayConstant(buyOrderBox.id)
+      )
+    )
+
+    val sellOrderContractParams =
+      DexSellerContractParameters(sellerPk, tokenId, tokenPrice, dexFeePerToken)
+
+    val sellOrderContract =
+      DexLimitOrderContracts.sellerContractInstance(sellOrderContractParams)
+
+    val sellOrderBox = ErgoBox(
+      value            = partialMatchTokenAmount * dexFeePerToken,
+      ergoTree         = sellOrderContract.ergoTree,
+      creationHeight   = 0,
+      additionalTokens = Seq((Digest32 @@ tokenId, partialMatchTokenAmount)),
+      additionalRegisters = Map(
+        ErgoBox.R4 -> ByteArrayConstant(tokenId),
+        ErgoBox.R5 -> LongConstant(tokenPrice)
+      )
+    )
+
+    val returnBox = ErgoBox(
+      value               = 1,
+      ergoTree            = buyerPk,
+      creationHeight      = 0,
+      additionalTokens    = Seq((Digest32 @@ tokenId, partialMatchTokenAmount)),
+      additionalRegisters = Map(ErgoBox.R4 -> ByteArrayConstant(buyOrderBox.id))
+    )
+
+    val txWithoutResidualBuyOrder = new ErgoLikeTransaction(
+      IndexedSeq(sellOrderBox).map(b => Input(b.id, ProverResult.empty)),
+      IndexedSeq(),
+      IndexedSeq(returnBox)
+    )
+
+    verifier
+      .verify(
+        buyOrderBox.ergoTree,
+        ErgoLikeContextTesting(
+          currentHeight       = 0,
+          lastBlockUtxoRoot   = AvlTreeData.dummy,
+          minerPubkey         = ErgoLikeContextTesting.dummyPubkey,
+          boxesToSpend        = IndexedSeq(sellOrderBox, buyOrderBox),
+          spendingTransaction = txWithoutResidualBuyOrder,
+          self                = buyOrderBox
+        ),
+        ProverResult.empty,
+        fakeMessage
+      )
+      .get
+      ._1 shouldBe false
   }
 }
